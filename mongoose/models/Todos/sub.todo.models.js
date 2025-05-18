a700faab3710da7543e914c094deb56df3f3cd87
Vipransh <vipransh231527@gmail.com>
2025-05-18 12:13:23 +0530

    Creation of Models Done

--- mongoose/models/Todos/sub.todo.models.js
+++ mongoose/models/Todos/sub.todo.models.js
@@ -1,3 +1,19 @@
-import mongoose from mongoose;
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({});
+const subTodoSchema = new mongoose.Schema({
+    content : { 
+        type: String,
+        required: true,
+
+    },
+    complete: {
+        type : Boolean,
+        default: false,
+    }
+    , createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    }
+})
+
+export const SubTodo = mongoose.model("SubTodo ", subTodoSchema)
