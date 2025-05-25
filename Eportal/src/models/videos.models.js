44ac69d2866d0aefbf87a3cab28478c6fadf9b81
Vipransh <vipransh231527@gmail.com>
2025-05-26 01:57:34 +0530

    Made User Video Models

---
+++ Eportal/src/models/videos.models.js
@@ -0,0 +1,47 @@
+import mongoose ,{Schema} from "mongoose";
+
+const videoSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        index: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    fullname:{
+        type: String,
+        required: true,
+        lowercase: true,
+        index: true,
+    },
+    avatar:{
+        type: String, //cloudinary
+        required: true,
+    },
+    avatar:{
+        type: String, //cloudinary
+    },
+    watchHistory:[{
+        type: Schema.Types.ObjectId,
+        ref : "Video"
+    }
+],
+    password: {
+        Type: String,
+        required: [true, "Password is Required"]
+    },
+    refreshTokens:{
+        type: String
+    }
+}
+, {
+    timestamps:true
+})
+
+export const User = mongoose.model("User" , userSchema)
