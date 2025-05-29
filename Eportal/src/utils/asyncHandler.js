69b324a01e81cedc2870972e1b407e6629c22669
Vipransh <vipransh231527@gmail.com>
2025-05-30 00:48:19 +0530

    debbugged the code

--- Eportal/src/utils/asyncHandler.js
+++ Eportal/src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (reqeustHandler) => {
-    (req,res,next) => {
+   return (req,res,next) => {
         Promise.resolve(reqeustHandler(req,res,next)).
         catch((err) => next(err))
     }
