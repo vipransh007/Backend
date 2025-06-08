ae4b3fc63fe7325cbbdd781b6b4a19640e752208
Vipransh <vipransh231527@gmail.com>
2025-06-09 01:23:59 +0530

    cookieRefresh

--- Eportal/src/controller/user.controllers.js
+++ Eportal/src/controller/user.controllers.js
@@ -116,9 +116,20 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-    User.findById(user._id);
+    await User.findByIdAndUpdate(req.user._id , {
+        $set:{
+            refreshTokens: undefined
+        }
+    })
+    const options = {
+        httpOnly : true,
+        secure: true
+    }
 
 })
 
+return res.status(200).clearCookie("accessToken", options)
+.clearCookie("refreshToken", options)
+.json(200, "User LoggedOut");
 
 export { registerUser, loginUser, logoutUser };
