fb5c53b274184542bc7536986b0c875a4cfd68db
Vipransh <vipransh231527@gmail.com>
2025-06-16 01:53:32 +0530

    Added user Routes and debbugged

--- Eportal/src/controller/user.controllers.js
+++ Eportal/src/controller/user.controllers.js
@@ -410,6 +410,7 @@ export {
     getCurrentUser,
     updateUserAvatar,
     updateAccountDetails,
+    updateUserCoverImage,
     getUserChannelProfile,
     getWatchHistory
 };
--- Eportal/src/routes/user.routes.js
+++ Eportal/src/routes/user.routes.js
@@ -1,7 +1,22 @@
-import {Router} from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controller/user.controllers.js";
-import {upload} from "../middlewares/multer.js"
+import { Router } from "express";
+import {
+    loginUser,
+    logoutUser,
+    registerUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+}
+    from "../controller/user.controllers.js";
+
+import { upload } from "../middlewares/multer.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { get } from "http";
 
 const router = Router()
 
@@ -12,7 +27,7 @@ router.route("/register").post(
     },
     {
         name: "coverImage",
-        maxCount:1
+        maxCount: 1
     }]),
     registerUser)
 
@@ -21,6 +36,13 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-Password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 
 
