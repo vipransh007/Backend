46d91484c99535a1e67c5fcbf9fb17c160f296e9
Vipransh <vipransh231527@gmail.com>
2025-06-20 15:33:39 +0530

    created Auth

---
+++ 12/src/appwrite/auth.js
@@ -0,0 +1,54 @@
+import conf from "../conf/config.js";
+
+import { Client, Account , ID} from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+    async createAccount({email , password, name}) {
+        try {
+            const userAccount= await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+
+                return this.login({email, password});
+            } else {
+                throw new Error("Account creation failed");
+            }
+        } catch (error) {
+            console.error("Error creating account:", error);
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+            if (session) {
+                return session;
+            } else {
+                throw new Error("Login failed");
+            }
+        } catch (error) {
+            console.error("Error logging in:", error);
+            throw error;
+        }
+    }
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.error("Error getting current user:", error);
+            throw error;
+        }
+    }
+}
+const authService = new AuthService();  
+
+export default authService;
