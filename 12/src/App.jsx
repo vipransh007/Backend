3ceaf934be8bcfd4b89045940b7f94f15b11f0e3
Vipransh <vipransh231527@gmail.com>
2025-06-21 16:05:34 +0530

    implement user authentication handling in App component

--- 12/src/App.jsx
+++ 12/src/App.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { use } from 'react';
 import './App.css';
+import { useState , useEffect} from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import {login , logout} from './store/authSlice';
+
+
 
 function App() {
-console.log(import.meta.env.VITE_REACT_APP_APPWRITE_URL);
+  // This is the main component of the app
+  // It renders a simple heading for the blog app
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if(userData){
+        dispatch(login({userData}));
+      }
+      else{
+        dispatch(logout());
+      }
+    }).finally(() => setLoading(false))
+   },[])
+
+   return !loading ? (
+    <div className='CondtionalRendering'></div>
+   ) : null;
+
 
   return (
     <>
